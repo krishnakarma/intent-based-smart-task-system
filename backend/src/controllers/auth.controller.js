f2123de5b315af656402fcf8ec0af3ed4f3ee4fc
Krishna Karma <karmakrishna1704@gmail.com>
2026-02-03 21:47:39 +0530

    feat(auth) : implement login with jwt

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,38 @@
 import bcrypt from "bcryptjs"
 import User from "../models/user.model.js"
+import generateToken from "../utils/generateToken.js"
+
+export const login = async(req , res , next)=>{
+    try {
+        const {email , password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({
+                message: "Email and password are required"
+            })
+        }
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({
+                message: "Invalid credentials",
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password , user.password);
+        if(!isMatch){
+            return res.status(401).json({
+                message: "Invalid credentials",
+            })
+        }
+        const token = generateToken(user._id);
+        res.status(200).json({
+            message: "Login successful",
+            token,
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
 
 export const signup = async(req , res , next)=>{
     try {
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { login , signup } from "../controllers/auth.controller.js";
 const router = Router();
 
 //signUp 
 router.post("/signup" , signup);
+router.post("/login" , login);
 //login
 router.post("/login" ,(req , res)=>{
     res.json({message: "Login route hit"})
